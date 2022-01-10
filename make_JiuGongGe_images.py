fe5c7c90f846682a36e012e11f8fa33db6a96263
SWHL <liekkaskono@163.com>
2022-01-10 10:03:34 +0800

    update files

--- make_JiuGongGe_images.py
+++ make_JiuGongGe_images.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+from pathlib import Path
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
 
-def obtain_heart(image_path):
-    """
-    将给定图像扣为心形
-    """
-    heart_im = cv2.imread('./assets/heart.jpg')
-
+def obtain_heart(bg_path, image_path):
+    """将给定图像扣为心形"""
+    
+    heart_im = cv2.imread(bg_path)
     heart_im = cv2.cvtColor(heart_im, cv2.COLOR_BGR2GRAY)
 
     im = cv2.imread(image_path)
-
     im = cv2.resize(im, (heart_im.shape[1], heart_im.shape[0]))
-
     im_back = np.zeros_like(im)
 
-    rows, cols = heart_im.shape
+    rows, cols = heart_im.shape[:2]
     for i in range(rows):
         for j in range(cols):
             if heart_im[i, j] == 0:
@@ -28,49 +24,56 @@ def obtain_heart(image_path):
             else:
                 im_back[i, j, :] = 255
 
-
     im_back = cv2.cvtColor(im_back, cv2.COLOR_BGR2RGB)
     return im_back
 
 
-def split_nine_images(im: np.array):
-    """
-    将图像分为九部分，并保存到对应路径下
-    """
+def split_nine_images(im, image_path):
+    """将图像分为九部分，并保存到对应路径下"""
+
     height, width = im.shape[:2]
-
-    height, width
-
     big_line = max(height, width)
 
-    newImg = np.zeros([big_line, big_line, 3], np.uint8) + 255
+    new_img = np.zeros([big_line, big_line, 3], np.uint8) + 255
 
     if height > width:
         edge = (big_line - width) // 2
-        newImg[:, edge: width+edge, :] = im
+        new_img[:, edge: width+edge, :] = im
     else:
         edge = (big_line - height) // 2
-        newImg[edge: height+edge, :, :] = im
+        new_img[edge: height+edge, :, :] = im
 
-    subHeight, subWidth = int(big_line / 3), int(big_line / 3)
+    sub_height, sub_width = int(big_line / 3), int(big_line / 3)
+
+    save_result_dir = Path('./assets/results') / Path(image_path).stem
+    save_result_dir.mkdir(parents=True, exists_ok=True)
 
     for i in range(3):
         for j in range(3):
             if i < 2:
                 if j < 2:
-                    tempImg = newImg[i*subHeight: (i+1)*subHeight, j*subWidth: (j+1)*subWidth, :]
+                    temp_img = new_img[i*sub_height: (i+1)*sub_height, j*sub_width: (j+1)*sub_width, :]
                 else:
-                    tempImg = newImg[i*subHeight: (i+1)*subHeight, j*subWidth:, :]
+                    temp_img = new_img[i*sub_height: (i+1)*sub_height, j*sub_width:, :]
             else:
                 if j < 2:
-                    tempImg = newImg[i*subHeight: , j*subWidth: (j+1)*subWidth, :]
+                    temp_img = new_img[i*sub_height: , j*sub_width: (j+1)*sub_width, :]
                 else:
-                    tempImg = newImg[i*subHeight: , j*subWidth:, :]
-            tempImg = cv2.cvtColor(tempImg, cv2.COLOR_BGR2RGB)
-            cv2.imwrite(f'./assets/images/{i*3+j}.jpg', tempImg)
+                    temp_img = new_img[i*sub_height: , j*sub_width:, :]
+            temp_img = cv2.cvtColor(temp_img, cv2.COLOR_BGR2RGB)
+
+            save_full_path = save_result_dir / f'{i * 3 + j}.jpg'
+            cv2.imwrite(str(save_full_path), temp_img)
 
 
-image_path =  './assets/1.jpg'
-result = obtain_heart(image_path)
-split_nine_images(result)
-print('九宫格图已经保存在/assets/images/下，序号顺序为从左到右')
+if __name__ == '__main__':
+    # 指定扣的形状
+    bg_path = './assets/background/heart.jpg'
+
+    # 原图
+    image_path =  './assets/raw_images/1.jpg'
+
+    result = obtain_heart(bg_path, image_path)
+
+    split_nine_images(result)
+    print('九宫格图已经保存在/assets/results/下，序号顺序为从左到右')
